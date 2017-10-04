c7bc63ba46130ea5b5025f2b8947a8d3aadd1c88
DESKTOP-LBHIUKJ\Antonio <cantonioq@gmail.com>
2017-10-04 10:30:41 +0100

    -created timer c# script -created ui for timer -are currently in template scene fully functional

---
+++ Xtreme Blitz/Assets/Scripts/Timer.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour {
+
+    public float timeLeft;
+    public Text timerDisplay;
+
+
+    void setTimerDisplay()
+    {
+        timerDisplay.text = "Time: " + timeLeft.ToString("0");
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        setTimerDisplay(); //refresh the display on screen function
+
+        timeLeft -= Time.deltaTime; 
+        if (timeLeft < 0 )
+        {
+            GameObject.Find("UI DATA").GetComponent<uiData>().lives -= 1.0f;
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+
+
+        if (GameObject.Find("UI DATA").GetComponent<uiData>().lives < 0.5f)
+        {
+            SceneManager.LoadScene("gameover");
+        }
+
+    }
+}
