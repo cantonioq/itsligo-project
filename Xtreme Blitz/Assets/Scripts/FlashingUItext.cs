dff090996b9e1c1fc1d32d770c845c21b6bfdd16
DESKTOP-LBHIUKJ\Antonio <cantonioq@gmail.com>
2017-10-04 19:40:01 +0100

    completed Title Screen + c# script

---
+++ Xtreme Blitz/Assets/Scripts/FlashingUItext.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FlashingUItext : MonoBehaviour {
+
+    public Text flashingText;
+    public float blinkSpeed;
+
+	// Use this for initialization
+	void Start () {
+
+        flashingText = gameObject.GetComponent<Text>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        flashingText.color = new Color(flashingText.color.r, flashingText.color.g, flashingText.color.b, Mathf.Sin(Time.time * blinkSpeed));
+
+    }
+}
---
+++ Xtreme Blitz/Assets/Scripts/titleScreen.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class titleScreen : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if ((Input.GetKeyDown("enter")) || (Input.GetKeyDown("joystick 1 button 7")) || (Input.GetKeyDown("joystick 2 button 7")))
+        {
+            Debug.Log("ENTER");
+            SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
+        }
+
+
+    }
+}
