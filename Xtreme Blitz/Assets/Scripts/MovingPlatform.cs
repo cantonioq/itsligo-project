6afc6e68a532aa0d019ab6740b44efae9dfbaa82
neon-HP\neon <goldenedge37@gmail.com>
2017-10-28 09:51:04 -0400

    Finished Level 2 added player textures

---
+++ Xtreme Blitz/Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    //public float movingSpeed = 10.0f;
+	void Update ()
+    {
+        transform.Translate(Vector3.left * Time.deltaTime);
+	}
+}
---
+++ Xtreme Blitz/Assets/Scripts/RotatingBarrel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatingBarrel : MonoBehaviour
+{
+    public float Rotatespeed = 10.0f;
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.Rotate(Vector3.down, Rotatespeed * Time.deltaTime);
+    }
+}
