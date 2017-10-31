24998d6ed88e6c0a3275cc5ba168b72155b8a4df
DESKTOP-LBHIUKJ\Antonio <cantonioq@gmail.com>
2017-10-31 12:10:53 +0000

    -updated battle scene 2 multiplayer -created script for collectible multiplayer(makes player bigger when he collects) -created prefab multiplayer battle collectible with prefab collectible (all in one)

---
+++ Xtreme Blitz/Assets/Scripts/multiplayer/MultiplayerScalingObject.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultiplayerScalingObject : MonoBehaviour {
+
+    public float scale;
+    public float minimumScale;
+    public float maximumScale;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.localScale += new Vector3(scale, 0, 0);
+
+        if (transform.localScale.x < minimumScale)
+        {
+            scale += 0.05f;
+        }
+
+        if (transform.localScale.x > maximumScale)
+        {
+            scale -= 0.05f;
+        }
+    }
+}
